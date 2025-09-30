7dbdf64b0e301b0388f2737a03052238d2ed5f03
pepperhot <lucas.gemo77@gmail.com>
2025-09-30 17:25:55 +0200

    optimisation js go

--- main.go
+++ main.go
@@ -9,17 +9,13 @@ import (
 )
 
 func main() {
-	// Créer un gestionnaire de fichiers personnalisé avec les bons MIME types
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Obtenir le chemin du fichier demandé
 		path := r.URL.Path
 		if path == "/" {
 			path = "/homepage.html"
 		}
 
-		// Déterminer le MIME type basé sur l'extension
-		ext := strings.ToLower(filepath.Ext(path))
-		switch ext {
+		switch strings.ToLower(filepath.Ext(path)) {
 		case ".css":
 			w.Header().Set("Content-Type", "text/css")
 		case ".js":
@@ -28,28 +24,18 @@ func main() {
 			w.Header().Set("Content-Type", "text/html")
 		}
 
-		// Servir le fichier
 		http.ServeFile(w, r, "."+path)
 	})
 
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	http.HandleFunc("/play", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "grille.html")
 	})
-
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-	http.HandleFunc("/click", clickHandler)
+	http.HandleFunc("/click", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Colonne cliquée :", r.URL.Query().Get("col"))
+		w.Write([]byte("OK"))
+	})
 
 	log.Println("Serveur lancé sur http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func clickHandler(w http.ResponseWriter, r *http.Request) {
-	col := r.URL.Query().Get("col")
-	row := r.URL.Query().Get("row")
-	fmt.Println("Colonne cliquée :", col)
-	fmt.Println("Ligne cliquée :", row)
-	w.Write([]byte("OK"))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- script.js
+++ script.js
@@ -1,49 +1,16 @@
 console.log("script.js chargé");
-document.addEventListener("DOMContentLoaded", function() {
-    
-    let cellules = document.querySelectorAll("td.cell");
 
-    cellules.forEach(function(cellule, _) {
-        
-        cellule.addEventListener("mouseover", function() {
-            surlignerColonne(cellule);
-        });
-        
-        cellule.addEventListener("mouseout", function() {
-            effacerSurlignage(cellule);
+document.addEventListener("DOMContentLoaded", () => {
+    const lignes = document.querySelectorAll("table tr");
+
+    lignes.forEach(tr => {
+        tr.querySelectorAll("td.cell").forEach((cell, colIndex) => {
+            // survol
+            cell.addEventListener("mouseover", () => lignes.forEach(l => l.cells[colIndex].style.background = "gray"));
+            cell.addEventListener("mouseout",  () => lignes.forEach(l => l.cells[colIndex].style.background = ""));
+            
+            // clic
+            cell.addEventListener("click", () => fetch(`/click?col=${colIndex+1}`).then(res => res.text()).then(console.log));
         });
     });
-    
-    function surlignerColonne(cellule) {
-        let colonneIndex = cellule.cellIndex;
-        
-        let lignes = document.querySelectorAll("table tr");
-        
-        lignes.forEach(function(ligne) {
-            let cases = ligne.querySelectorAll("td");
-            cases[colonneIndex].style.background = "gray";
-        });
-    }
-    
-    function effacerSurlignage(cellule) {
-        let colonneIndex = cellule.cellIndex;
-        let lignes = document.querySelectorAll("table tr");
-        
-        lignes.forEach(function(ligne) {
-            let cases = ligne.querySelectorAll("td");
-            cases[colonneIndex].style.background = "";
-        });
-    }
-});
-
-const rows = document.querySelectorAll('tr');
-rows.forEach((tr, rowIndex) => {
-  const cells = tr.querySelectorAll('.cell');
-  cells.forEach((cell, colIndex) => {
-    cell.addEventListener('click', () => {
-      fetch(`/click?row=${rowIndex+1}&col=${colIndex+1}`)
-        .then(res => res.text())
-        .then(console.log);
-    });
-  });
 });
